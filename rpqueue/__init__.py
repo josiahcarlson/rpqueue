83d9fede69c4ccf619dcb47436396f0be252df9b
Sergey Makovsky <sergey.makovskyy@chownow.com>
2019-08-05 13:29:22 -0700

    Change version file to 0.27.2

--- rpqueue/__init__.py
+++ rpqueue/__init__.py
@@ -46,7 +46,7 @@ import redis
 if list(map(int, redis.__version__.split('.'))) < [2, 4, 12]:
     raise Exception("Upgrade your Redis client to version 2.4.12 or later")
 
-VERSION = '0.27.1'
+VERSION = '0.27.2'
 
 RPQUEUE_CONFIGS = {}
 
